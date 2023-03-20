aea4544ee486324b8ef58f7f5772cbcb9e5f0ca8
Akash kumar <65840057+AKASHKUMAR507@users.noreply.github.com>
2023-03-20 23:46:12 +0530

    second

---
+++ ReactNative/screen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet,Text} from 'react-native';
+
+const Screen = () => {
+    return (
+        <View>
+            <Text>test</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Screen;
