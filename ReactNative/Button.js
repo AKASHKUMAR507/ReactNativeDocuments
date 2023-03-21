3aeaa637706a87a7a42f670f14d2d2d1647d2f7e
Akash kumar <65840057+AKASHKUMAR507@users.noreply.github.com>
2023-03-21 10:50:21 +0530

    ButtonNavigaton

--- ReactNative/Button.js
+++ ReactNative/Button.js
@@ -1,39 +1,112 @@
-import React,{useState} from 'react';
-import {View, StyleSheet,Text} from 'react-native';
+// write a program to display two components in same page in react native
 
-const Email =()=>{
-    return(
-        <View>
-            <Text>Email</Text>
-            <TextInput
-                plcaeholder = {'Enter Email Id'}
-            />
-        </View>
-    );
-}
+import React from 'react';
+import { View } from 'react-native';
 
-const Phone = () =>{
-    return(
-        <>
-            <Text>
-                Phone
-            </Text>
-            <TextInput
-                plcaeholder={'Enter Phone Number'}
-            />
-        </>
-    );
-}
+const Component1 = () => {
+  return (
+    <View>
+      // component 1 JSX
+    </View>
+  );
+};
+
+const Component2 = () => {
+  return (
+    <View>
+      // component 2 JSX
+    </View>
+  );
+};
 
 const Button = () => {
-    const [tab , setTab] = useState();
-    return (
-        <View>
-            <Text>Hello Akash Your are set</Text>
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({})
+  return (
+    <View>
+      <Component1 />
+      <Component2 />
+    </View>
+  );
+};
 
 export default Button;
+
+
+// write a program to display two components with button in same place react native
+
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Component1 = () => {
+  return (
+    <View>
+      <Text>This is component 1</Text>
+    </View>
+  );
+};
+
+const Component2 = () => {
+  return (
+    <View>
+      <Text>This is component 2</Text>
+    </View>
+  );
+};
+
+const Button = () => {
+  const [showComponent2, setShowComponent2] = useState(false);
+
+  const handleButtonPress = () => {
+    setShowComponent2(!showComponent2);
+  };
+
+  return (
+    <View>
+      <Component1 />
+      {showComponent2 && <Component2 />}
+      <Button title={showComponent2 ? "Hide Component 2" : "Show Component 2"} onPress={handleButtonPress} />
+    </View>
+  );
+};
+
+export default Button;
+
+
+
+// write a program to display two components with button if button is press then replace first components with second components in react native
+
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+
+const Component1 = () => {
+  return (
+    <View>
+      <Text>This is component 1</Text>
+    </View>
+  );
+};
+
+const Component2 = () => {
+  return (
+    <View>
+      <Text>This is component 2</Text>
+    </View>
+  );
+};
+
+const Button = () => {
+  const [showComponent2, setShowComponent2] = useState(false);
+
+  const handleButtonPress = () => {
+    setShowComponent2(!showComponent2);
+  };
+
+  return (
+    <View>
+      {showComponent2 ? <Component2 /> : <Component1 />}
+      <Button title={showComponent2 ? "Show Component 1" : "Show Component 2"} onPress={handleButtonPress} />
+    </View>
+  );
+};
+
+export default Button;
+
