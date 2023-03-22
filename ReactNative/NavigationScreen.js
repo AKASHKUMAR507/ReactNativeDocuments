94c3d07557e57e1c14764eafb5410a21ff021f0d
Akash kumar <65840057+AKASHKUMAR507@users.noreply.github.com>
2023-03-22 13:08:51 +0530

    Create NavigationScreen.js

---
+++ ReactNative/NavigationScreen.js
@@ -0,0 +1,11 @@
+      <View style={styles.container}>
+        <Text>You are on the home page</Text>
+//On button click, we will navigate to the about page.
+        <Button
+            title="About page"
+            onPress={() => this.props.navigation.navigate('About')}
+        />// Button has two props i.e title and onPress
+// On onPress, we are calling the navigate function on this.props.navigation.
+//As our HomeScreen is included in the stack.navigator so this.props.navigation will inherit props from the navigation object.
+
+      </View>
